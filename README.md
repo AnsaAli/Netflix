a89cf8986205d9170775f3d28b82e2a81cfcac2e
AnsaAli <chinjuansa@example.com>
2024-07-23 10:51:28 +0530

    Validation added

--- README.md
+++ README.md
@@ -1,8 +1,13 @@
 # Netflix gpt
 create react app
 configure tailwindcss
+Header
+created routes
+Login Form
+Signup form
+Form validation
+useRef hook
 
-# features
--browse 
-  - hreader
-  -
+# to get the values from input
+1. using useState
+2. USing useRef
--- src/component/Login.js
+++ src/component/Login.js
@@ -1,13 +1,25 @@
 
 import Header from "./Header";
-import { useState } from "react";
-
+import { useRef, useState } from "react";
+import validateData from "../utils/validate"
 const Login = () => {
     const [isSigninForm, setIsSignInForm] = useState(true);
+    const [errorMessage, setErrorMessage] = useState(null);
 
-    const toggleForm =()=>{
+    const email = useRef(null);
+    const password = useRef(null)
+
+    const toggleForm = () => {
         setIsSignInForm(!isSigninForm)
 
+    }
+
+    const handleButtonClick = () => {
+    //   console.log('emai',email.current.value); will get in inspect
+    //   console.log(password);
+    const message = validateData(email.current.value, password.current.value)
+    // console.log(message)
+    setErrorMessage(message);
     }
     return (
         <div >
@@ -17,30 +29,41 @@ const Login = () => {
                     src="https://assets.nflxext.com/ffe/siteui/vlv3/655a9668-b002-4262-8afb-cf71e45d1956/5ff265b6-3037-44b2-b071-e81750b21783/IN-en-20240715-POP_SIGNUP_TWO_WEEKS-perspective_WEB_c6d6616f-4478-4ac2-bdac-f54b444771dd_small.jpg" />
 
             </div>
-            <form className="absolute rounded-lg bg-opacity-80 p-12 bg-black w-3/12 my-24 mx-auto right-0 left-0 text-white">
+            {/* to prevent the form submit  onSubmit={(e)=> e.preventDefault()} */}
+            <form 
+            onSubmit={(e)=> e.preventDefault()}
+            className="absolute rounded-lg bg-opacity-80 p-12 bg-black w-3/12 my-24 mx-auto right-0 left-0 text-white">
                 <h1 className="text-3xl py-4">
                     {isSigninForm ? "Sign In" : "Sign Up"}
-                    </h1>
-                    {!isSigninForm && (
-                         <input
-                         type="text"
-                         placeholder="Your name"
-                         className="p-4 my-4 w-full  bg-gray-700 " />
-                    )}
+                </h1>
+                <p className="text-red-500 text-lg p-2">{errorMessage}</p>
+
+                {!isSigninForm && (
+                    <input
+                        type="text"
+                        placeholder="Your name"
+                        className="p-4 my-4 w-full  bg-gray-700 " />
+                )}
                 <input
                     type="text"
+                    ref={email}
                     placeholder="Enter email"
                     className="p-4 my-4 w-full  bg-gray-700 " />
                 <input
                     type="password"
+                    ref={password}
                     placeholder="Enter your password"
                     className="p-4 my-4 w-full bg-gray-700" />
+
+
                 <button
-                    className="my-4 p-4 bg-red-700 w-full rounded-lg" >{isSigninForm ? "Sign In" : "Sign Up"}
+                    className="my-4 p-4 bg-red-700 w-full rounded-lg"
+                    onClick={handleButtonClick} >
+                    {isSigninForm ? "Sign In" : "Sign Up"}
                 </button>
                 <p className="p-4 text-gray-400 cursor-pointer" onClick={toggleForm}>
-                 {isSigninForm ? "New to Netflix? Sign up now." : "Alredy regestered? Sign in now."}
-                    </p>
+                    {isSigninForm ? "New to Netflix? Sign up now." : "Alredy regestered? Sign in now."}
+                </p>
             </form>
 
         </div>
